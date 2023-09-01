306233395af2736d568b631d9933b57b96b2f3df
Kalyan Kumar Reddy Chitiki (kchitiki) <kchitiki@cisco.com>
2023-09-01 16:39:58 +0530

    Update main.tf

--- modules/vpc_modules/main.tf
+++ modules/vpc_modules/main.tf
@@ -63,7 +63,7 @@ resource "aws_eip" "cisco_ise_nat_ips" {
 
 resource "aws_vpc_dhcp_options" "cisco_ise_dhcp_options" {
   domain_name_servers = ["AmazonProvidedDNS"]
-  domain_name        = "ec2.internal"
+  domain_name        = "drilldevops.in"
   tags = {
     Name = "cisco_ise_DHCPOptions"
   }
