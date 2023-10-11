082fccb11578c7c8221e2f680cc49fdef7d07428
Akash <akashgu2@cisco.com>
2023-10-11 11:54:20 +0530

    updating readme.md

--- backend.tf
+++ backend.tf
@@ -1,8 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "ise-terraform-test"
     key     = "terraform/vpc/vpc.tfstate" 
-    region  = "us-east-2"
     encrypt = "true"
     # Optionally, you can specify DynamoDB table for state locking
     # dynamodb_table = "your-dynamodb-table"
