b75a2823bd989dc9ba1d7e2c80c6dc55e18c8080
kchitiki <kchitiki@cisco.com>
2023-10-25 11:50:53 +0530

    removing region in variables.tf and creating .tfvars

--- modules/vpc_modules/variables.tf
+++ modules/vpc_modules/variables.tf
@@ -1,88 +1,91 @@
+######################################################################################
+#######################     Block to add AWS VPC variables   #########################
+######################################################################################
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = ""
 }
 
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  default     = ""
 }
 
 variable "vpc_name" {
   description = "Name tag for the VPC"
   type        = string
-  default     = "cisco_ise"
+  default     = ""
 }
 
 variable "enable_dns_support" {
   description = "Whether to enable DNS support for the VPC"
   type        = bool
-  default     = true
+  default     = null
 }
 
 variable "enable_dns_hostnames" {
   description = "Whether to enable DNS hostnames for the VPC"
   type        = bool
-  default     = true
+  default     = null
 }
 
 variable "availability_zones" {
   description = "List of availability zones"
   type        = list(string)
-  default     = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  default     = ""
 }
 
 variable "public_subnet_cidrs" {
   description = "List of CIDR blocks for public subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default     = ""
 }
 
 variable "private_subnet_cidrs" {
   description = "List of CIDR blocks for private subnets"
   type        = list(string)
-  default     = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+  default     = ""
 }
 
 variable "internet_gateway_name" {
   description = "Name tag for the Internet Gateway"
   type        = string
-  default     = "Cisco_ISE_IGW"
+  default     = ""
 }
 variable "dhcp_domain_name" {
   description = "Domain name for DHCP options"
   type        = string
-  default     = "ec2.internal"
+  default     = ""
 }
 
 variable "create_nat_gateways" {
   description = "Create NAT Gateways for public subnets"
   type        = bool
-  default     = true
+  default     = null
 }
 
 variable "create_eips" {
   description = "Create Elastic IPs for NAT Gateways"
   type        = bool
-  default     = true
+  default     = null
 }
 
 variable "domain_name_servers" {
   description = "List of DNS servers to use"
   type        = list(string)
-  default     = ["169.254.169.253"]  # Default DNS server(s)
+  default     = ""      # Default DNS server(s)
 }
 
 variable "ntp_servers" {
   description = "List of NTP servers to use"
   type        = list(string)
-  default     = ["169.254.169.123"]  # Default NTP server(s)
+  default     = ""  # Default NTP server(s)
 }
 
 variable "dhcp_options_domain_name" {
   description = "Domain name for DHCP options"
   type        = string
-  default     = "drilldevops.in"     # Default domain name
+  default     = ""     # Default domain name
 }
